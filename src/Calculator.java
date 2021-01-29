d776758e895120ec262796f8f2e623c704cbb8f1
nazimudheen267 <nazimudheen267@gmail.com>
2021-01-29 12:16:20 -0500

    add listener added into the programme

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,28 +1,80 @@
 
-	import java.awt.Color;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-	import javax.swing.JLabel;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-	public class Calculator {
 
-	public Calculator(){
+public class  Calculator implements ActionListener{
+
+	
+		//global 
+	
+		JFrame frame;
+		
+		JLabel DisplayLabel;
+		
+		JButton SevenButton;
+		
+		JButton EightButton;
+		
+		JButton NineButton;
+		
+		JButton FourButton;
+		
+		JButton FiveButton;
+
+		
+		JButton SixButton;
+
+		JButton OneButton;
+		
+		JButton TwoButton;
+		
+		JButton ThreeButton;
+		
+		JButton ZeroButton;
+		
+		JButton DotButton;
+		
+		JButton EqualButton;
+		
+		JButton AdditionButton;
+					
+		JButton SubstractionButton;
+		
+		JButton MultiplicationButton;
+				
+		JButton divitionButton;
+		
+		JButton PercentageButton;
+		
+		JButton ClearButton;
+
+		JButton AllClearButton;
+		
+		
+		
+			public Calculator(){
 		
 			// frame 
 		
-		JFrame frame=new JFrame("Calculator") ;
+		frame=new JFrame("Calculator") ;
 		frame.setLayout(null);
 		frame.setSize(800,700);
 		frame.setLocation(300,50);
 		
 		
 		
+		
 		// label 
 		
-		JLabel DisplayLabel=new JLabel("hello");
+		DisplayLabel=new JLabel("hello");
 		DisplayLabel.setBounds(50, 50, 680, 50);
 		DisplayLabel.setBackground(Color.gray);
 		DisplayLabel.setOpaque(true);
@@ -39,116 +91,134 @@ import javax.swing.SwingConstants;
 		
 		//Buttons
 		
-		JButton SevenButton=new JButton("7");
+		SevenButton=new JButton("7");
 		SevenButton.setBounds(180,230,80,80);
 		SevenButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		SevenButton.setBackground(Color.white);
+		SevenButton.addActionListener(this);
 		
-		JButton EightButton=new JButton("8");
+		EightButton=new JButton("8");
 		EightButton.setBounds(280,230,80,80);
 		EightButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		EightButton.setBackground(Color.white);
+		EightButton.addActionListener(this);
 		
 		
-		JButton NineButton=new JButton("9");
+		NineButton=new JButton("9");
 		NineButton.setBounds(380,230,80,80);
 		NineButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		NineButton.setBackground(Color.white);
+		NineButton.addActionListener(this);
 		
-		JButton FourButton=new JButton("4");
+		FourButton=new JButton("4");
 		FourButton.setBounds(180,320,80,80);
 		FourButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		FourButton.setBackground(Color.white);
+		FourButton.addActionListener(this);
 		
-		JButton FiveButton=new JButton("5");
+		FiveButton=new JButton("5");
 		FiveButton.setBounds(280,320,80,80);
 		FiveButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		FiveButton.setBackground(Color.white);
+		FiveButton.addActionListener(this);
 		
 		
-		JButton SixButton=new JButton("6");
+		SixButton=new JButton("6");
 		SixButton.setBounds(380,320,80,80);
 		SixButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		SixButton.setBackground(Color.white);
+		SixButton.addActionListener(this);
 		
-		JButton OneButton=new JButton("1");
+		OneButton=new JButton("1");
 		OneButton.setBounds(180,410,80,80);
 		OneButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		OneButton.setBackground(Color.white);
+		OneButton.addActionListener(this);
 		
-		JButton TwoButton=new JButton("2");
+		TwoButton=new JButton("2");
 		TwoButton.setBounds(280,410,80,80);
 		TwoButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		TwoButton.setBackground(Color.white);
+		TwoButton.addActionListener(this);
 		
-		
-		JButton ThreeButton=new JButton("3");
+		ThreeButton=new JButton("3");
 		ThreeButton.setBounds(380,410,80,80);
 		ThreeButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		ThreeButton.setBackground(Color.white);
+		ThreeButton.addActionListener(this);
 		
-		JButton ZeroButton=new JButton("0");
+		ZeroButton=new JButton("0");
 		ZeroButton.setBounds(180,500,180,80);
 		ZeroButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		ZeroButton.setBackground(Color.white);
+		ZeroButton.addActionListener(this);
 		
-		JButton DotButton=new JButton(".");
+		DotButton=new JButton(".");
 		DotButton.setBounds(380,500,80,80);
 		 DotButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		 DotButton.setBackground(Color.white);
+		 DotButton.addActionListener(this);
 		
 		
 		
 		//operator buttons
 		
-		JButton EqualButton=new JButton("=");
+		EqualButton=new JButton("=");
 		EqualButton.setBounds(480,500,80,80);
 		EqualButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		EqualButton.setBackground(Color.green);
 		EqualButton.setForeground(Color.white);
+		EqualButton.addActionListener(this);
 		
 		
-		JButton AdditionButton=new JButton("+");
+		AdditionButton=new JButton("+");
 		AdditionButton.setBounds(480,410,80,80);
 		AdditionButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		AdditionButton.setBackground(Color.white);
+		AdditionButton.addActionListener(this);
 		
 			
-		JButton SubstractionButton=new JButton("-");
+		SubstractionButton=new JButton("-");
 		SubstractionButton.setBounds(480,230,80,80);
 		SubstractionButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		SubstractionButton.setBackground(Color.white);
+		SubstractionButton.addActionListener(this);
 		
-		JButton MultiplicationButton=new JButton("*");
+		MultiplicationButton=new JButton("*");
 		MultiplicationButton.setBounds(480,320,80,80);
 		MultiplicationButton.setFont(new Font("Arial", Font.PLAIN, 	30));
 		MultiplicationButton.setBackground(Color.white);
+		MultiplicationButton.addActionListener(this);
 		
 		
 		
 		
-		JButton divitionButton=new JButton("/");
+		divitionButton=new JButton("/");
 		divitionButton.setBounds(480,140,80,80);
 		divitionButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		divitionButton.setBackground(Color.white);
+		divitionButton.addActionListener(this);
 		
 		
-		JButton PercentageButton=new JButton("%");
+		PercentageButton=new JButton("%");
 		PercentageButton.setBounds(380,140,80,80);
 		PercentageButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		PercentageButton.setBackground(Color.white);
+		PercentageButton.addActionListener(this);
 		
-		JButton ClearButton=new JButton("C");
+		ClearButton=new JButton("C");
 		ClearButton.setBounds(280,140,80,80);
 		ClearButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		ClearButton.setBackground(Color.yellow);
 		ClearButton.setForeground(Color.white);
+		ClearButton.addActionListener(this);
 		
-		JButton AllClearButton=new JButton("AC");
+		AllClearButton=new JButton("AC");
 		AllClearButton.setBounds(180,140,80,80);
 		AllClearButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		AllClearButton.setBackground(Color.red);
 		AllClearButton.setForeground(Color.white);
+		AllClearButton.addActionListener(this);
 		
 		
 		
@@ -185,5 +255,49 @@ import javax.swing.SwingConstants;
 		
 	}
 
+		
+		public void actionPerformed(ActionEvent e){
+			if (e.getSource()==SevenButton) {
+				DisplayLabel.setText("7");
+			}else if (e.getSource()==EightButton) {
+				DisplayLabel.setText("8");
+			}else if(e.getSource()==NineButton) {
+				DisplayLabel.setText("9");
+			}else if(e.getSource()==FourButton) {
+				DisplayLabel.setText("4");
+			}
+			else if(e.getSource()==FiveButton) {
+				DisplayLabel.setText("5");
+			}
+			else if(e.getSource()==SixButton) {
+				DisplayLabel.setText("6");
+			}
+			
+			else if(e.getSource()==OneButton) {
+				DisplayLabel.setText("1");
+			}
+			else if(e.getSource()==TwoButton) {
+				DisplayLabel.setText("2");
+			}
+			else if(e.getSource()==ThreeButton) {
+				DisplayLabel.setText("3");
+			}
+			else if(e.getSource()==ZeroButton) {
+				DisplayLabel.setText("0");
+			}else if (e.getSource()==AdditionButton) {
+				DisplayLabel.setText("+");
+			}else if(e.getSource()==MultiplicationButton) {
+				DisplayLabel.setText("*");
+			}else if(e.getSource()==SubstractionButton) {
+				DisplayLabel.setText("-");
+			}
+			else if(e.getSource()==divitionButton) {
+				DisplayLabel.setText("/");
+			}
+			else if(e.getSource()==PercentageButton) {
+				DisplayLabel.setText("%");
+			}
+			
+		}
 	}
 
