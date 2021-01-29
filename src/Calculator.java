c6a0daa111c84465e44f377226260e1bc6b45d13
nazimudheen267 <nazimudheen267@gmail.com>
2021-01-29 02:46:37 -0500

    calculator text field(label) added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,44 @@
 
-	import javax.swing.JFrame;
+	import java.awt.Color;
+
+import javax.swing.JFrame;
+	import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 	public class Calculator {
 
 	public Calculator(){
+		
+			// frame 
+		
 		JFrame frame=new JFrame("Calculator") ;
 		frame.setLayout(null);
 		frame.setSize(600,600);
+		frame.setLocation(300,100);
+		
+		
+		
+		// label 
+		
+		JLabel DisplayLabel=new JLabel("hello");
+		DisplayLabel.setBounds(30, 50, 540, 50);
+		DisplayLabel.setBackground(Color.gray);
+		DisplayLabel.setOpaque(true);
+		DisplayLabel.setForeground(Color.white);
+		
+		DisplayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		
+		//added label into the frame 
+		
+		frame.add(DisplayLabel);
+		
+		
+		
 		
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 	}
