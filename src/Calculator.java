93ad9a709bd556d27be915cec1cee5fdb763d1b2
nazimudheen267 <nazimudheen267@gmail.com>
2021-01-29 02:18:33 -0500

    calculator frame created

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+
+	import javax.swing.JFrame;
+
+	public class Calculator {
+
+	public Calculator(){
+		JFrame frame=new JFrame("Calculator") ;
+		frame.setLayout(null);
+		frame.setSize(600,600);
+		
+		frame.setVisible(true);
+	}
+
+	}
+
